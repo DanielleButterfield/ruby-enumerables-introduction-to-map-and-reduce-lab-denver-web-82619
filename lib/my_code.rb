aa5ec38fef5a07bed7ff52af3fd7c8b6ae59c011
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-22 07:38:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-  total = 0 + starting_point
+  total = starting_point
   count = 0
   while source_array[count] do
     total += source_array[count]
